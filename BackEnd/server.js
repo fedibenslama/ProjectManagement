e40f6a90dda3283674d7922029d8aa1c564cd930
Your Name <fedi.benslama@esprit.tn>
2022-07-27 00:30:49 +0100

    Completed The Crud For ProjectTable

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const knex = require('knex')
+const knex = require('knex');
+const { response } = require("express");
 
 db = knex({
     client: 'pg',
@@ -20,59 +21,62 @@ db = knex({
 app.use(cors());
 app.use(express.json());
 
-// const database = {
-//     projects: [
-//         {
-//             id: "1",
-//             Name: "Project1",
-//             Type: "Web Developement",
-//             UsedSolutions: "React,JavaScript,NodeJS,MySQL",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client1",
-//             Status: "Under Development"
-//         },
-//         {
-//             id: "2",
-//             Name: "Project2",
-//             Type: "Mobile Application",
-//             UsedSolutions: "Flutter,React",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client2",
-//             Status: "Under Development"
-//         },
-//         {
-//             id: "3",
-//             Name: "Project3",
-//             Type: "Data Science",
-//             UsedSolutions: "Machine Learning,Deep Learning,Python",
-//             AssociatedServers: "Production Server",
-//             AssociatedClient: "Client3",
-//             Status: "Testing"
-//         },
-//         {
-//             id: "4",
-//             Name: "Project4",
-//             Type: "Software Development",
-//             UsedSolutions: "Java,Python",
-//             AssociatedServers: "Test Server",
-//             AssociatedClient: "Client4",
-//             Status: "Validated"
-//         },
-//         {
-//             id: "5",
-//             Name: "Project5",
-//             Type: "Cloud Computing",
-//             UsedSolutions: "Machine Learning,AWS",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client5",
-//             Status: "Testing"
-//         }
-//     ]
-// }
+const database = {
+    projects: [
+        {
+            id: "1",
+            Name: "Project1",
+            Type: "Web Developement",
+            UsedSolutions: "React,JavaScript,NodeJS,MySQL",
+            AssociatedServers: "Development Server",
+            AssociatedClient: "Client1",
+            Status: "Under Development"
+        },
+        {
+            id: "2",
+            Name: "Project2",
+            Type: "Mobile Application",
+            UsedSolutions: "Flutter,React",
+            AssociatedServers: "Development Server",
+            AssociatedClient: "Client2",
+            Status: "Under Development"
+        },
+        {
+            id: "3",
+            Name: "Project3",
+            Type: "Data Science",
+            UsedSolutions: "Machine Learning,Deep Learning,Python",
+            AssociatedServers: "Production Server",
+            AssociatedClient: "Client3",
+            Status: "Testing"
+        },
+        {
+            id: "4",
+            Name: "Project4",
+            Type: "Software Development",
+            UsedSolutions: "Java,Python",
+            AssociatedServers: "Test Server",
+            AssociatedClient: "Client4",
+            Status: "Validated"
+        },
+        {
+            id: "5",
+            Name: "Project5",
+            Type: "Cloud Computing",
+            UsedSolutions: "Machine Learning,AWS",
+            AssociatedServers: "Development Server",
+            AssociatedClient: "Client5",
+            Status: "Testing"
+        }
+    ]
+}
 
 // -------------Get All Projects-------------------
 app.get('/projects', (req, res) => {
-    res.send(database.projects)
+    db.select('*').from('projects').then(projects => {
+        res.json(projects);
+    })
+
 })
 
 // -------------Get one Project-------------------
@@ -125,45 +129,88 @@ app.post('/addProject', (req, res) => {
 })
 // ------------------ Edit A Project ------------------
 app.put('/EditProject', (req, res) => {
-    let found = false;
-    const { id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body
-    database.projects.forEach(project => {
-        if (project.id === id) {
-            found = true;
-            const index = database.projects.findIndex((project) => project.id === id);
-            database.projects[index] = {
-                Name: Name,
-                Type: Type,
-                UsedSolutions: UsedSolutions,
-                AssociatedServers: AssociatedServers,
-                AssociatedClient: AssociatedClient,
-                Status: Status
-            }
+    
 
-            return res.json(database.projects[index]);
-        }
+    const {id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body; //to avoid repeating req.body
+    db('projects')
+        .returning('*')  //our response is all the columns
+        .where('id', '=', id)
+        .update({
+            // be careful it's case sensitive , in the db all are lowercase
+            name: Name,
+            type: Type,
+            usedsolutions: UsedSolutions,
+            associatedservers: AssociatedServers,
+            associatedclient: AssociatedClient,
+            status: Status,
+        }).then(project => {
+            res.json(project[0]);
+        })
+        .catch(err => {
+            res.status(400).json('Unable To Edit Project')  //giving the error details in not good for security so i'll change the err with smthing else
+        })
 
-    })
-    if (!found) {
-        res.status(404).json("Project Not Found")
-    }
+
+    // database.projects.push({
+    //     id: id,
+    //     Name: Name,
+    //     Type: Type,
+    //     UsedSolutions: UsedSolutions,
+    //     AssociatedServers: AssociatedServers,
+    //     AssociatedClient: AssociatedClient,
+    //     Status: Status,
+    // })
+    // res.json(database.projects[database.projects.length - 1]);
+
 })
+    // let found = false;
+    // const { id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body
+    // database.projects.forEach(project => {
+    //     if (project.id === id) {
+    //         found = true;
+    //         const index = database.projects.findIndex((project) => project.id === id);
+    //         database.projects[index] = {
+    //             Name: Name,
+    //             Type: Type,
+    //             UsedSolutions: UsedSolutions,
+    //             AssociatedServers: AssociatedServers,
+    //             AssociatedClient: AssociatedClient,
+    //             Status: Status
+    //         }
+
+    //         return res.json(database.projects[index]);
+    //     }
+
+    // })
+    // if (!found) {
+    //     res.status(404).json("Project Not Found")
+    // }
+
 // ------------------ Delete A Project ------------------
 app.put('/DeleteProject', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.projects.forEach(project => {
-        if (project.id === id) {
-            found = true;
-            const index = database.projects.findIndex((project) => project.id === id);
-            database.projects.splice(index, 1)
-            return res.json(database.projects);
-        }
+    db('projects')
+        .where('id', '=', id)
+        .del(
+            '*'
+        )
+        .then(response => {
+            res.json("Deleted")
+        })
+        .catch(err => res.status(400).json('Unable To Delete'))
+    // let found = false;
+    // database.projects.forEach(project => {
+    //     if (project.id === id) {
+    //         found = true;
+    //         const index = database.projects.findIndex((project) => project.id === id);
+    //         database.projects.splice(index, 1)
+    //         return res.json(database.projects);
+    //     }
 
-    })
-    if (!found) {
-        res.status(404).json("Project Not Found")
-    }
+    // })
+    // if (!found) {
+    //     res.status(404).json("Project Not Found")
+    // }
 })
 
 
